b8c0a86508f285f3dceea398a30672015a59b667
Bohdan Olenin <bohdan.olenin@oa.edu.ua>
2026-02-02 20:14:33 +0200

    changed ToolAssignment interface

--- src/Application/Common/Interfaces/Repositories/IToolAssigmentsRepository.cs
+++ src/Application/Common/Interfaces/Repositories/IToolAssigmentsRepository.cs
@@ -5,6 +5,7 @@ namespace Application.Common.Interfaces.Repositories
 {
     public interface IToolAssigmentsRepository
     {
-        Task<ToolAssignment> Approve(CancellationToken cancellationToken);
+        Task<ToolAssignment> AddAsync(ToolAssignment entity, CancellationToken cancellationToken);
+        Task<ToolAssignment> DeleteAsync(ToolAssignment entity, CancellationToken cancellationToken);
     }
 }
